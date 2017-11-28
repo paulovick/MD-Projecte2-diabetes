6e4ece382be01ce8aa3171cc826472fdc7d58adf
Marc Cubiró <marc.cubiro@est.fib.upc.edu>
2017-11-28 12:36:05 +0100

    separat en funcions

--- svm.py
+++ svm.py
@@ -41,61 +41,20 @@ def svm(df):
     print("Confusion matrix on test set:\n", sklearn.metrics.confusion_matrix(y_test, pred))
     print("\nAccuracy on test set: ", sklearn.metrics.accuracy_score(y_test, pred))
 
-    # findClineal(X_train, X_test, y_train, y_test)
-
-    # Cs = np.logspace(-3, 11, num=15, base=10.0)
-
-    # param_grid = {'C': Cs}
-    # grid_search = GridSearchCV(SVC(kernel='poly', degree=2), param_grid, cv=10)
-    # grid_search.fit(X_train, y_train)
-    #
-    # scores = grid_search.cv_results_['mean_test_score']
-    #
-    # plt.semilogx(Cs, scores)
-    # plt.show()
-    #
-    # parval = grid_search.best_params_
-    # knc = SVC(kernel='poly', degree=2, C=parval['C'])
-    # knc.fit(X_train, y_train)
-    # pred = knc.predict(X_test)
-    # print("Confusion matrix on test set:\n", sklearn.metrics.confusion_matrix(y_test, pred))
-    # print("\nAccuracy on test set: ", sklearn.metrics.accuracy_score(y_test, pred))
-    # print("\nBest combination of parameters found: ", parval)
-    # print("\nNumber of supports: ", np.sum(knc.n_support_), "(", np.sum(np.abs(knc.dual_coef_) == parval['C']),
-    #       "of them have slacks)")
-    # print("Prop. of supports: ", np.sum(knc.n_support_) / X_train.shape[0])
-
     knc = SVC()
     knc.fit(X_train, y_train)
     pred = knc.predict(X_test)
     print("Confusion matrix on test set:\n", sklearn.metrics.confusion_matrix(y_test, pred))
     print("\nAccuracy on test set: ", sklearn.metrics.accuracy_score(y_test, pred))
 
-    # # Values we will test for each parameter. When observin results, consider the limits of the
-    # # values tested and increase them if necessary
-    # gammas = [0.000001, 0.00001, 0.0001, 0.001, 0.01, 0.1, 1, 10]
-    # Cs = np.logspace(-1, 6, num=8, base=10.0)
-    #
-    # param_grid = {'C': Cs, 'gamma': gammas}
-    # grid_search = GridSearchCV(SVC(), param_grid, cv=10)
-    # grid_search.fit(X_train, y_train)
-    # parval = grid_search.best_params_
-    #
-    # # We'll show in a grid, the accuracy for each combination of parameters tester
-    # scores = grid_search.cv_results_['mean_test_score']
-    # scores = np.array(scores).reshape(len(param_grid['C']), len(param_grid['gamma']))
-    #
-    # plt.matshow(scores)
-    # plt.xlabel('gamma')
-    # plt.ylabel('C')
-    # plt.colorbar()
-    # plt.xticks(np.arange(len(param_grid['gamma'])), param_grid['gamma'], rotation='vertical')
-    # plt.yticks(np.arange(len(param_grid['C'])), param_grid['C'])
-    # plt.show()
-    # print("\nBest combination of parameters found: ", parval)
+    findCLineal(X_train, X_test, y_train, y_test)
+
+    findCPoly(X_train, X_test, y_train, y_test)
+
+    findC(X_train, y_train)
 
 
-def findClineal(X_train, X_test, y_train, y_test):
+def findCLineal(X_train, X_test, y_train, y_test):
     # List of C values to test. We usualy test diverse orders of magnitude
     # Cs = np.logspace(-3, 11, num=15, base=10.0)
     Cs = np.logspace(-3, 5, num=9, base=10.0)
@@ -127,3 +86,50 @@ def findClineal(X_train, X_test, y_train, y_test):
     pred = knc.predict(X_test)
     print("Confusion matrix on test set:\n", sklearn.metrics.confusion_matrix(y_test, pred))
     print("\nAccuracy on test set: ", sklearn.metrics.accuracy_score(y_test, pred))
+
+def findCPoly(X_train, X_test, y_train, y_test):
+    Cs = np.logspace(-3, 11, num=15, base=10.0)
+
+    param_grid = {'C': Cs}
+    grid_search = GridSearchCV(SVC(kernel='poly', degree=2), param_grid, cv=10)
+    grid_search.fit(X_train, y_train)
+
+    scores = grid_search.cv_results_['mean_test_score']
+
+    plt.semilogx(Cs, scores)
+    plt.show()
+
+    parval = grid_search.best_params_
+    knc = SVC(kernel='poly', degree=2, C=parval['C'])
+    knc.fit(X_train, y_train)
+    pred = knc.predict(X_test)
+    print("Confusion matrix on test set:\n", sklearn.metrics.confusion_matrix(y_test, pred))
+    print("\nAccuracy on test set: ", sklearn.metrics.accuracy_score(y_test, pred))
+    print("\nBest combination of parameters found: ", parval)
+    print("\nNumber of supports: ", np.sum(knc.n_support_), "(", np.sum(np.abs(knc.dual_coef_) == parval['C']),
+          "of them have slacks)")
+    print("Prop. of supports: ", np.sum(knc.n_support_) / X_train.shape[0])
+
+def findC(X_train, y_train):
+    # Values we will test for each parameter. When observin results, consider the limits of the
+    # values tested and increase them if necessary
+    gammas = [0.000001, 0.00001, 0.0001, 0.001, 0.01, 0.1, 1, 10]
+    Cs = np.logspace(-1, 6, num=8, base=10.0)
+
+    param_grid = {'C': Cs, 'gamma': gammas}
+    grid_search = GridSearchCV(SVC(), param_grid, cv=10)
+    grid_search.fit(X_train, y_train)
+    parval = grid_search.best_params_
+
+    # We'll show in a grid, the accuracy for each combination of parameters tester
+    scores = grid_search.cv_results_['mean_test_score']
+    scores = np.array(scores).reshape(len(param_grid['C']), len(param_grid['gamma']))
+
+    plt.matshow(scores)
+    plt.xlabel('gamma')
+    plt.ylabel('C')
+    plt.colorbar()
+    plt.xticks(np.arange(len(param_grid['gamma'])), param_grid['gamma'], rotation='vertical')
+    plt.yticks(np.arange(len(param_grid['C'])), param_grid['C'])
+    plt.show()
+    print("\nBest combination of parameters found: ", parval)
