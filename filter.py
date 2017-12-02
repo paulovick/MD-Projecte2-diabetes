44e1ca480f72f1856c341381a849ae776ae3d049
Dani Criado <dani.criado.casas@gmail.com>
2017-12-02 09:31:51 +0100

    Split dataset into train and test stratifying

--- filter.py
+++ filter.py
@@ -1,48 +1,31 @@
-#from naive_bayes import naive_bayes
-#from svm import svm
-#from KNN import executeKNN
+# from naive_bayes import naive_bayes
+# from svm import svm
+# from KNN import executeKNN
 from decisionTree import decisionTree
 from preprocessing import *
 from KNN import *
 from adaboost import *
-#dataset = read_and_filter_dataset(preprocess=True,nrows=6000,save_csv=True)
-dataset = read_and_filter_dataset(use_preprocessed=True)
+
+
+def splitting(ds, nrows):
+    y_name = "readmitted"  # volem predir la columna readmitted, aixi que sera la nostre y
+    X = ds.drop(['readmitted'], axis=1)
+    y = ds[y_name]
+    (X1, Xresta, y1, yresta) = cv.train_test_split(X, y, train_size=nrows, random_state=1, stratify=y)
+    return X1, y1
+
+
+dataset = read_and_filter_dataset(preprocess=True, save_csv=True)
+# dataset = read_and_filter_dataset(use_preprocessed=True)
+X, y = splitting(dataset, nrows=5000)
 
 # plot_statistics(dataset)
 # plot_null_statistics(dataset)
 
-#naive_bayes(dataset)
-#svm(dataset)
+# naive_bayes(dataset)
+# svm(dataset)
 if __name__ == "__main__":
-	#sense la linia de dalt, executeKNN quedara en loop infinit
-	#executeKNN(dataset)
-	#decisionTree(dataset)
-	adaboost(dataset)
-
-# from sklearn import preprocessing
-# import matplotlib.pyplot as plt
-
-# encoder = preprocessing.LabelEncoder()
-
-# encoder.fit(y)
-# # plt.scatter(X[:,0], y, c=encoder.transform(y), cmap=plt.cm.Paired)
-# # plt.show()
-
-# from sklearn import cross_validation
-# from sklearn.tree import DecisionTreeClassifier
-# from sklearn import metrics
-# from os import system
-
-# X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.3, random_state=0)
-# # print(X_train.shape)
-# # print(X_test.shape)
-
-# from sklearn.tree import DecisionTreeClassifier
-
-# clf = DecisionTreeClassifier(random_state=0).fit(X_train, y_train)
-# clf.score(X_test, y_test)     
-# y_pred = clf.predict(X_test)
-# print "Classification Report:"
-# print metrics.classification_report(y_test, y_pred)
-# print "Confusion Matrix:"
-# print metrics.confusion_matrix(y_test, y_pred)
+    # sense la linia de dalt, executeKNN quedara en loop infinit
+    # executeKNN(dataset)
+    # decisionTree(dataset)
+    adaboost(dataset)
